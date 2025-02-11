11936a34be462c4a4419cc02a5d949f008ffc7b5
LucasJenkins731 <lgjenkins@quinnipiac.edu>
2025-02-10 21:37:49 -0500

    Asks for two whole numbers, then prints results of added numbers

--- calculator.java
+++ calculator.java
@@ -1,5 +1,18 @@
-public class calculator{
-    public static void main(String[] args){
+import java.util.*;
 
+public class calculator{
+    public static int a;
+    public static int b;
+    public static int results;
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        //Ask for user input
+        System.out.printf("Please enter your first number: ");
+        a = scanner.nextInt();
+        System.out.printf("Please enter your second number: ");
+        b = scanner.nextInt();
+        results = a + b;
+        //Shows addition
+        System.out.printf("%d + %d = %d", a, b, results);
     }
 }
